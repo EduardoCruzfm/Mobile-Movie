6633ac4a91c8a654ba8d05f7b352a633983ec741
Eduardo Cruz <edu_cm_95@hotmail.com>
2023-01-26 09:33:38 -0300

    Peticion de tendencias

--- src/main.js
+++ src/main.js
@@ -1,1 +1,39 @@
-console.log(API_KEY);
+async function getTrendingMoviesPreview() {
+    const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY);
+    const data = await res.json();
+
+    const movies = data.results;
+    // console.log({data,movies});
+    
+    movies.forEach(movie => {
+
+        // Seleccionamos el secction contenedor(padre) por #ID y y al article
+        // contenedor (hijo) por .CLASE donde lo inyectaremos con un appen
+        const trendingPreviewMoviesContainer = document.querySelector
+        ("#trendingPreview .trendingPreview-movieList");
+
+        // Creamos etiquetas Html y le inyectamos las clases
+        const movieContainer = document.createElement("div");
+        movieContainer.classList.add("movie-container");
+
+        const movieImg = document.createElement("img");
+        movieImg.classList.add("movie-img");
+
+        // Le agregamos un atributo (tipo de atributo, volor del atributo) titulo
+        movieImg.setAttribute("alt", movie.title);
+
+        // Le agregamos un atributo (tipo de atributo, volor del atributo) imagen
+        movieImg.setAttribute(
+            "src", "https://image.tmdb.org/t/p/w500/" + movie.poster_path
+            );
+
+        // Inyectamos a movieImg al contenedor movieContainer (Agremamos la img)
+        movieContainer.appendChild(movieImg);
+
+        // Inyectamos movieContainer(div) al contenedor 
+        // trendingPreviewMoviesContainer(article)
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+    
+}
+getTrendingMoviesPreview();
